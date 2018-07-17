cc0c00e5cae8a47c01a8e2be7380fce2adcf4ac1
unknown <fordrsmax@gmail.com>
2018-07-17 19:53:57 +0200

    primara manipulacion de imagenes

--- static/gametable-rotacion.js
+++ static/gametable-rotacion.js
@@ -1,5 +1,8 @@
 var socket = io();
+var image = new Image();
+image.src = '/static/ferrari.png';
 
+//var image = document.getElementById('coche');
 var canvas = document.getElementById('canvas');
 this.canvas.width = window.innerWidth;
 this.canvas.height = window.innerHeight;
@@ -14,9 +17,11 @@ socket.on('state', function(players) {
     var player = players[id];
     context.save();
     context.translate(player.x, player.y);
+    //context.fillStyle = player.color;
+    //context.fillRect(40 / -2, 40 / -2,40, 40);
+    context.translate(image.width/2,image.height/2);
     context.rotate(player.angle);
-    context.fillStyle = player.color;
-    context.fillRect(40 / -2, 40 / -2,40, 40);
+    context.drawImage(image, -image.width/2, -image.height/2, 36, 103);
     context.restore();
     //buelvo a color blanco por si hay errores
     context.fillStyle = 'white';
